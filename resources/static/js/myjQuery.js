359161dde223e29135eaa53b68532cccac18a85e
Rajkumar Krishnan <58025939+KCTech-io@users.noreply.github.com>
2020-12-08 11:43:45 +0530

    Update myjQuery.js

--- resources/static/js/myjQuery.js
+++ resources/static/js/myjQuery.js
@@ -1,11 +1,68 @@
-function displayLogin(username) {
-  // The 4th line makes objects visible in the for loop.
-  // Determines if pre-requisites are met and if so - makes div draggable
-  // for new courses 
+function myFunction(){
+    alert("Button Pressed");
+    }
+function volLogin() {
+}
 
-if (username = "") {
+function changeActiveMenu(menuname) {
 
- 
-      $('#test-button').css("background-color", "white");
+
+	var myElement = document.getElementById('top_menu');
+	
+
+   var myMenuList = myElement.getElementsByTagName('li');
+   
+  
+   for (i = 0; i < myMenuList.length; i++) {
+   if ((myMenuList[i].id == menuname) && (myMenuList[i].id != 'menu_right'))
+   {
+   		myMenuList[i].classList.add('active');
+   		myMenuList[i].classList.remove('not-active');
+   		}
+   else
+   {
+   		myMenuList[i].classList.remove('active');
+   		myMenuList[i].classList.add('not-active');
+   		}
+   		}
+}
+
+function openTranslate(articleID,userID) {
+    window.location.assign("/translate/article?articleid="+articleID+"&userid="+userID);
+    
+}
+
+function openNav() {
+    document.getElementById("menu-side-bar").style.width = "200px";
+    document.getElementById("main-menu").style.marginLeft = "200px";
+}
+function closeNav() {
+    document.getElementById("menu-side-bar").style.width = "0";
+	document.getElementById("main-menu").style.marginLeft= "0";
+}
+
+function loginDisplay(username,loginerrormess) {
+    if(username == "") {
+  				
+        document.getElementById("login-icon").style.visibility='visible';
+        document.getElementById("login-icon").style.display='block';
+        document.getElementById("logged-user-icon").style.visiblity='hidden';
+        document.getElementById("logged-user-icon").style.display='none';
+
+		if (loginerrormess == "Success") {
+	        document.getElementById("auth-error").style.visibility='hidden';
+			document.getElementById("auth-error").style.display='none';
+	        }
+		else {
+			document.getElementById("auth-error").style.visibility='visible';
+			document.getElementById("auth-error").style.display='block';
+		}
+		
+    } else {
+
+        document.getElementById("login-icon").style.visibility='hidden';
+        document.getElementById("login-icon").style.display='none';
+        document.getElementById("logged-user-icon").style.visiblity='visible';
+        document.getElementById("logged-user-icon").style.display='block';
     }
 }
